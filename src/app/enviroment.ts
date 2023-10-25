632c332c48cc326cfe831cc92780cf1b2ba1ac00
Joao Marques Desktop Windows <johnmarques.br2002@gmail.com>
2023-10-25 14:03:16 -0400

    path for publish

--- src/app/enviroment.ts
+++ src/app/enviroment.ts
@@ -1,2 +1,2 @@
-export const BASEURL: string = 'http://localhost:8080/api';
-export const PDFURL = 'http://localhost:8080/ReportPDF?venid=';
+export const BASEURL: string = '/api';
+export const PDFURL = '/ReportPDF?venid=';
